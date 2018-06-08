661ff52542629cf234e5d76bc35527c6b7a6a389
Wister <fujianifan@gmail.com>
2018-06-08 19:04:25 +0900

    TweetAlarm

--- main.py
+++ main.py
@@ -13,20 +13,41 @@ class Win(QDialog):
 
     def __init__(self, parent=None):
         super(Win, self).__init__(parent)
+        self.flag = 0
+        self.al = datetime.time(19, 1)
+        self.CFrom()
+        self.ButSig('Pushed')
+        self.TimeSig()
+
+    def CFrom(self):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+
+    def ButSig(self, state):
         self.ui.pushButton.clicked.connect(lambda:
-                                           Twipost.Twi_aupos('Pushed'))
+                                           Twipost.Twi_aupos(state))
         self.ui.pushButton_2.clicked.connect(app.quit)
+
+    def TimeSig(self):
         self.timer = QTimer(self)
         self.timer.timeout.connect(lambda:
                                    self.ui.lineEdit.
                                    setText(datetime.
                                            datetime.now().
                                            strftime('%H:%M:%S')))
-        self
+        self.timer.timeout.connect(lambda:
+                                   self.Alarm('TIME!!'))
         self.timer.start(1000)  # ミリ秒単位
 
+    def Alarm(self, state):
+        self.ti = datetime.datetime.now()
+        if(self.ti.minute == self.al.minute):
+            if(self.flag == 0):
+                self.flag = 1
+                if(self.ti.hour == self.al.hour):
+                    Twipost.Twi_aupos(state)
+        else:
+            self.flag = 0
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
