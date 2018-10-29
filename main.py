323965a07c7b9f811245a6d68fda39e2c6cec81a
Wister <fujianifan@gmail.com>
2018-10-29 17:52:00 +0900

    Basic function is almost completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -8,84 +9,98 @@ import SetAlarm
 import datetime
 import subprocess
 import Twipost
+from pygame import mixer
+import time
+import threading
 
 al = datetime.time(6, 00)
+music = ''
 with open('AlarmTime.ti', 'r') as ti:
-        tim = ti.read().split(':')
-        al = datetime.time(int(tim[0]),
-                           int(tim[1]))
+    tim = ti.read().split(':')
+    al = datetime.time(int(tim[0]),
+                       int(tim[1]))
+with open('musicpath.txt', 'r') as mu:
+    music = mu.readline()
 
 
-class mainwin(QDialog):
+def musicPlay(musicpath):
+    mixer.init()
+    mixer.music.load(musicpath)
+    mixer.music.play(-1)
+    time.sleep(15)
+    mixer.music.stop()
+thread_1 = threading.Thread(target=lambda: musicPlay(music))
+
+
+class MainWindow(QDialog):
 
     def __init__(self, parent=None):
-        super(mainwin, self).__init__(parent)
+        super(MainWindow, self).__init__(parent)
         self.flag = 0
         self.timer = pyqtSignal()
-        self.CFrom()
-        self.ButSig()
-        self.TimeSig()
+        self.createForm()
+        self.buttonSignal()
+        self.timerSignal()
 
-    def CFrom(self):
+    def createForm(self):
         self.ui = clockform.Ui_Form()
         self.ui.setupUi(self)
 
-    def ButSig(self):
-        self.ui.pushButton.clicked.connect(self.MakeWin)
+    def buttonSignal(self):
+        self.ui.pushButton.clicked.connect(self.makeWindow)
         self.ui.pushButton_2.clicked.connect(app.quit)
 
-    def TimeSig(self):
+    def timerSignal(self):
         self.timer = QTimer(self)
+        self.timer.timeout.connect(lambda: self.ui.lineEdit.setText(
+                                   datetime.datetime.now()
+                                   .strftime('%H:%M:%S')))
         self.timer.timeout.connect(lambda:
-                                   self.ui.lineEdit.
-                                   setText(datetime.
-                                           datetime.now().
-                                           strftime('%H:%M:%S')))
-        self.timer.timeout.connect(lambda:
-                                   self.Alarm())
+                                   self.alarm())
         self.timer.start(200)  # ミリ秒単位
 
-    def Alarm(self):
+    def alarm(self):
         self.ti = datetime.datetime.now()
         if(self.ti.minute == al.minute):
-            if(self.flag == 0):
+            if self.flag == 0:
                 self.flag = 1
                 if(self.ti.hour == al.hour):
-                    subprocess.call('aplay Test.wav', shell=True)
-                    subprocess.call('aplay Test.wav', shell=True)
+                    thread_1.start()
         elif(self.ti.minute == (al.minute+5) % 60):
-            if(self.flag == 0):
+            if self.flag == 0:
                 self.flag = 1
-                if(self.ti.hour == al.hour or
-                   (self.ti.minute >= 55 and self.ti.hour-1 == al.hour)):
-                    subprocess.call('aplay Test.wav', shell=True)
-                    subprocess.call('aplay Test.wav', shell=True)
+                if ((self.ti.minute <= 4 and self.ti.hour-1 == al.hour) or
+                   (self.ti.minute >= 5 and self.ti.hour == al.hour)):
                     Twipost.Twi_aupos('アラームから遅れてるっぽい')
         else:
             self.flag = 0
 
-    def MakeWin(self):
-        self.sub = SubWin()
+    def makeWindow(self):
+        self.sub = SubWindow()
         self.sub.show()
 
 
-class SubWin(QDialog):
+class SubWindow(QDialog):
     def __init__(self, parent=None):
-        super(SubWin, self).__init__(parent)
+        super(SubWindow, self).__init__(parent)
         self.ui = setting.Ui_Form()
         self.ui.setupUi(self)
-        self.clbutton()
-        self.Butsig()
+        self.closeWindow()
+        self.buttonSignal()
 
-    def clbutton(self):
+    def closeWindow(self):
         self.ui.pushButton_3.clicked.connect(self.close)
 
-    def Butsig(self):
+    def buttonSignal(self):
         self.ui.pushButton.clicked.connect(self.makeAlarm)
+        self.ui.pushButton_2.clicked.connect(self.musicDialog)
 
     def makeAlarm(self):
-        self.Alarm = AlarmSetting()
-        self.Alarm.show()
+        self.alarm = AlarmSetting()
+        self.alarm.show()
+
+    def musicDialog(self):
+        self.music = SelectFile()
 
 
 class AlarmSetting(QDialog):
@@ -93,38 +108,38 @@ class AlarmSetting(QDialog):
         super(AlarmSetting, self).__init__(parent)
         self.ui = SetAlarm.Ui_Form()
         self.ui.setupUi(self)
-        self.clbutton()
-        self.ButSig()
+        self.closeWindow()
+        self.buttonSignal()
         self.SH = 0
         self.SM = 0
 
-    def clbutton(self):
-        self.ui.pushButton_5.clicked.connect(self.Savetime)
+    def closeWindow(self):
+        self.ui.pushButton_5.clicked.connect(self.saveTime)
         self.ui.pushButton_5.clicked.connect(self.close)
 
-    def ButSig(self):
-        self.ui.pushButton.clicked.connect(self.Hinc)
-        self.ui.pushButton_2.clicked.connect(self.Hdec)
-        self.ui.pushButton_3.clicked.connect(self.Minc)
-        self.ui.pushButton_4.clicked.connect(self.Mdec)
+    def buttonSignal(self):
+        self.ui.pushButton.clicked.connect(self.hourIncrement)
+        self.ui.pushButton_2.clicked.connect(self.hourDecrement)
+        self.ui.pushButton_3.clicked.connect(self.minuteIncrement)
+        self.ui.pushButton_4.clicked.connect(self.minuteDecrement)
 
-    def Hinc(self):
+    def hourIncrement(self):
         self.SH = (self.SH+1) % 24
         self.ui.label.setText(str(self.SH))
 
-    def Hdec(self):
+    def hourDecrement(self):
         self.SH = (self.SH+23) % 24
         self.ui.label.setText(str(self.SH))
 
-    def Minc(self):
+    def minuteIncrement(self):
         self.SM = (self.SM+1) % 60
         self.ui.label_2.setText(str(self.SM))
 
-    def Mdec(self):
+    def minuteDecrement(self):
         self.SM = (self.SM+59) % 60
         self.ui.label_2.setText(str(self.SM))
 
-    def Savetime(self):
+    def saveTime(self):
         global al
         al = datetime.time(self.SH, self.SM)
         with open('AlarmTime.ti', 'w') as f:
@@ -132,8 +147,19 @@ class AlarmSetting(QDialog):
             f.write(self.state)
 
 
+class SelectFile(QMainWindow):
+    def __init__(self, parent=None):
+        super(SelectFile, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        fname = QFileDialog.getOpenFileName(self, 'Select mp3', '/home/wister')
+        if fname[0]:
+            music = fname[0]
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = mainwin()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
